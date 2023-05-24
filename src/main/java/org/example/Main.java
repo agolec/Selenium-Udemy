145f84fa8396517f83ab2e667db0bf532f06c769
Adam Golec <agolec@nexient.com>
2023-05-24 13:03:41 -0400

    set up system properties to read from user directory to retrieve the chrome driver.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final String CHROME_DRIVER = "webdriver.chrome.driver";
+        System.setProperty(CHROME_DRIVER, System.getProperty("user.dir") + "/src/main/java/org/example/core/drivers/chromedriver.exe");
+
         ChromeDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com");
+        System.out.println(driver.getTitle());
+
     }
 }
