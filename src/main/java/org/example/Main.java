1e76eb3da3e67dfd564497365866248ae0fd02e8
Adam Golec <agolec@nexient.com>
2023-05-25 11:49:59 -0400

    Add edge driver into core.drivers directory.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,25 @@
 package org.example;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
 
 public class Main {
     public static void main(String[] args) {
-        final String CHROME_DRIVER = "webdriver.chrome.driver";
-        System.setProperty(CHROME_DRIVER, System.getProperty("user.dir") + "/src/main/java/org/example/core/drivers/chromedriver.exe");
+        //final String CHROME_DRIVER = "webdriver.chrome.driver";
+        final String GECKO_DRIVER = "webdriver.gecko.driver";
+        //chromedriver setup
+        //System.setProperty(CHROME_DRIVER, System.getProperty("user.dir") + "/src/main/java/org/example/core/drivers/chromedriver.exe");
 
-        ChromeDriver driver = new ChromeDriver();
+       // WebDriver driver = new ChromeDriver();
+
+        //Firefox Launch
+        WebDriver driver = new FirefoxDriver();
+
         driver.get("https://rahulshettyacademy.com");
         System.out.println(driver.getTitle());
+        driver.quit();
 
     }
 }
