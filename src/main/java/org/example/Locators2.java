111bdb22d7a3e06a1f67bc30ff10f17eae339bdd
Adam <algolec1990@yahoo.com>
2026-02-15 11:46:41 -0500

    set up basic happy path flow for login and assertion.

--- src/main/java/org/example/Locators2.java
+++ src/main/java/org/example/Locators2.java
@@ -1,2 +1,52 @@
-package org.example;public class Locators2 {
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.xml.sax.Locator;
+
+import java.time.Duration;
+
+
+
+public class Locators2 {
+
+    public static void main(String[] args) throws InterruptedException {
+
+// TODO Auto-generated method stub
+
+//implicit wait - 2 seconds time out
+
+
+        final String CHROME_DRIVER = "webdriver.chrome.driver";
+        System.setProperty(CHROME_DRIVER, System.getProperty("user.dir") + "/src/main/java/org/example/core/drivers/chromedriver.exe");
+
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+
+        driver.findElement(By.id("inputUsername")).sendKeys("rahul");
+
+        driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+
+        driver.findElement(By.className("signInBtn")).click();
+
+        System.out.println(driver.findElement(By.xpath("//p[text()='You are successfully logged in.']")).getText());
+
+        String expectedString = "You are successfully logged in.";
+
+        WebElement locator = driver.findElement(By.xpath("//p[text()='"+ expectedString + "']"));
+        String actualString = locator.getText();
+        Assert.assertEquals(actualString,expectedString);
+
+
+
+    }
+
+
 }
