693de79db1925cb1472bacc826226b0a8b040f56
Adam <algolec1990@yahoo.com>
2026-02-26 20:26:34 -0500

    change the import package for Assert on line 28.

--- src/main/java/org/example/Checkboxes.java
+++ src/main/java/org/example/Checkboxes.java
@@ -1,6 +1,6 @@
 package org.example;
 
-import dev.failsafe.internal.util.Assert;
+import org.testng.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -25,7 +25,7 @@ public class Checkboxes {
         boolean expectedSelection = true;
         boolean actualSelection = seniorCitizen.isSelected();
 
-        Assert.isTrue(expectedSelection,"Error. Element not selected");
+        Assert.assertTrue(expectedSelection,"Error. Element not selected");
         System.out.println("end program");
 
         int expectedCheckboxCount = 6;
