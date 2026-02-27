3834d3b0e3809a7890c2d1b4189732a5d25eccfd
Adam <algolec1990@yahoo.com>
2026-02-26 20:48:05 -0500

    add exercise 1

---
+++ src/main/java/org/example/CheckboxesExercise.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import org.testng.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class CheckboxesExercise {
+    public static void main(String[] args) throws InterruptedException {
+        final String CHROME_DRIVER = "webdriver.chrome.driver";
+        System.setProperty(CHROME_DRIVER, System.getProperty("user.dir") + "/src/main/java/org/example/core/drivers/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+        WebElement firstCheckbox = driver.findElement(By.cssSelector("#checkBoxOption1"));
+
+        Assert.assertFalse(firstCheckbox.isSelected());
+        firstCheckbox.click();
+        Thread.sleep(2000);
+        Assert.assertTrue(firstCheckbox.isSelected());
+
+        System.out.println("END PROGRAM");
+
+    }
+}
