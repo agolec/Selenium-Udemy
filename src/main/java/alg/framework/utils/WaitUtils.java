0bebccc547fa1498398b9fc0ac6cede0b94dee75
Adam <algolec1990@yahoo.com>
2026-03-12 11:38:23 -0400

    set up wait utilities in WaitUtils for browser waits.

--- src/main/java/alg/framework/utils/WaitUtils.java
+++ src/main/java/alg/framework/utils/WaitUtils.java
@@ -1,2 +1,17 @@
-package alg.framework.utils;public class WaitUtils {
+package alg.framework.utils;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.time.Duration;
+
+public class WaitUtils {
+    public static void waitForElementToBeVisible(WebDriver driver, By locator, int secondsOfWaiting){
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(secondsOfWaiting));
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
 }
