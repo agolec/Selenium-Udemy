62f461ea34c6eb1afd769e76c5d4b3a2c172f068
Adam <algolec1990@yahoo.com>
2026-03-12 11:37:47 -0400

    modify the DriverFactory to begin reading the browser from ConfigReader and switch case on the browser set in the config file.

--- src/main/java/alg/framework/driver/DriverFactory.java
+++ src/main/java/alg/framework/driver/DriverFactory.java
@@ -1,16 +1,34 @@
 package alg.framework.driver;
 
+import alg.framework.config.ConfigReader;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.time.Duration;
 
 public class DriverFactory {
     public static WebDriver createDriver() {
-        final String CHROME_DRIVER = "webdriver.chrome.driver";
-        System.setProperty(CHROME_DRIVER, System.getProperty("user.dir") + "/src/main/java/org/example/core/drivers/chromedriver.exe");
+        String browser = ConfigReader.getProperty("browser");
 
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver;
+
+        switch(browser.toLowerCase()){
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")
+                        + "/src/main/java/org/example/core/drivers/chromedriver.exe" );
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                System.setProperty("webdriver.gecko.driver",
+                        System.getProperty("user.dir")
+                                + "/src/main/java/org/example/core/drivers/geckodriver.exe");
+                driver = new FirefoxDriver();
+                break;
+            default:
+                throw new RuntimeException("unsupported browser");
+        }
+
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
