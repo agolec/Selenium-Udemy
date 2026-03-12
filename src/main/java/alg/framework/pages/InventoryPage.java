09ee9661d26eaec8402c01a7342299206ab2beef
Adam <algolec1990@yahoo.com>
2026-03-12 12:17:21 -0400

    Add inventory page to have example of site navigating to another page.

--- src/main/java/alg/framework/pages/InventoryPage.java
+++ src/main/java/alg/framework/pages/InventoryPage.java
@@ -1,2 +1,16 @@
-package alg.framework.pages;public class InventoryPage {
+package alg.framework.pages;
+
+import alg.framework.base.BasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class InventoryPage extends BasePage {
+    private By inventoryContainer = By.id("inventory_container");
+
+    public InventoryPage(WebDriver driver){
+        super(driver);
+    }
+    public boolean isInventoryPageDisplayed(){
+        return isElementDisplayed(inventoryContainer);
+    }
 }
