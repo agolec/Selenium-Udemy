be3ce299189a1d481a01349d00c8579b3b736386
Adam <algolec1990@yahoo.com>
2026-03-12 10:54:37 -0400

    add code that sets up proof of concept for code that will instanciate the WebDriver class.

--- src/main/java/alg/framework/driver/DriverFactory.java
+++ src/main/java/alg/framework/driver/DriverFactory.java
@@ -1,2 +1,20 @@
-package alg.framework.driver;public class DriverFactory {
+package alg.framework.driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class DriverFactory {
+    public static WebDriver createDriver() {
+        final String CHROME_DRIVER = "webdriver.chrome.driver";
+        System.setProperty(CHROME_DRIVER, System.getProperty("user.dir") + "/src/main/java/org/example/core/drivers/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        driver.manage().window().maximize();
+        return driver;
+    }
 }
