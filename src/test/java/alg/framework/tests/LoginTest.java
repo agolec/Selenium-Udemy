c111b09294bd6ba3f0d5217df2a1fd1ed2b18586
Adam <algolec1990@yahoo.com>
2026-03-12 10:56:04 -0400

    add in the first test for Login proof of concept.

--- src/test/java/alg/framework/tests/LoginTest.java
+++ src/test/java/alg/framework/tests/LoginTest.java
@@ -1,2 +1,15 @@
-package alg.framework.tests;public class LoginTest {
+package alg.framework.tests;
+
+import alg.framework.base.BaseTest;
+import alg.framework.pages.LoginPage;
+import org.testng.annotations.Test;
+
+public class LoginTest extends BaseTest {
+
+    @Test
+    public void validLoginTest(){
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.loginToApplication("standard_user","password");
+    }
+
 }
