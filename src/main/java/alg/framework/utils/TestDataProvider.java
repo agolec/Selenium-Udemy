fc04c93afd822c6f2994fe5ea541cc7a47945db1
Adam <algolec1990@yahoo.com>
2026-03-12 11:41:56 -0400

    add test data provider to read in data sets for tests.

---
+++ src/main/java/alg/framework/utils/TestDataProvider.java
@@ -0,0 +1,14 @@
+package alg.framework.utils;
+
+import org.testng.annotations.DataProvider;
+
+public class TestDataProvider {
+    @DataProvider(name = "loginData")
+    public Object[][] loginData() {
+        return new Object [][] {
+                {"standard_user", "secret_sauce"},
+                {"locked_out_user","secret_sauce"},
+                {"problem_user","secret_sauce"}
+        };
+    }
+}
