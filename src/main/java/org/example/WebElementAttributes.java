2d27ba5676cb7194a81d546f82fd363c895ee65f
Adam <algolec1990@yahoo.com>
2026-03-23 15:49:44 -0400

    add in comments to explain why .isEnabled() is not the go to method choice for the test.

--- src/main/java/org/example/WebElementAttributes.java
+++ src/main/java/org/example/WebElementAttributes.java
@@ -20,6 +20,10 @@ public class WebElementAttributes extends Config {
         driver.get(LOCATOR_PRACTICE);
 
         System.out.println(getAttributeValue(ROUND_TRIP_BTN,"style"));
+        //This element is enabled as far as selenium is concerned. We cannot do a findElement().isEnabled() check on it.
+        //however, on this element, we have a style attribute, and the opacity value of this attribute changes with
+        //the state of the page. On load, it is 0.5, and when either this element is clicked on, or round trip selected,
+        //the opacity value of the style attribute becomes 1.
         Assert.assertTrue(getAttributeValue(ROUND_TRIP_BTN,"style").contains(EXPECTED_VALUE_DISABLED));
         clickElement(ROUND_TRIP_BTN);
         Assert.assertTrue(getAttributeValue(ROUND_TRIP_BTN,"style").contains(EXPECTED_VALUE_ENABLED));
