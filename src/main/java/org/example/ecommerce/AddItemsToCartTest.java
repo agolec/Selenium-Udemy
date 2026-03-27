9f5814877131a500767b16f816ad3b8b58e957ee
Adam <algolec1990@yahoo.com>
2026-03-27 17:19:00 -0400

    add a Items To Cart test. Test avoids using text of an element to find the element necessary to interact with.

--- src/main/java/org/example/ecommerce/AddItemsToCartTest.java
+++ src/main/java/org/example/ecommerce/AddItemsToCartTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class AddItemsToCartTest extends Config {
@@ -15,20 +16,32 @@ public class AddItemsToCartTest extends Config {
     void test(){
         driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+        String[] vegetablesToFind = {"Brocolli","Cucumber","Beetroot"};
 
         //Add Cucumber into the cart. Generically setup the test so we can chose any item?
         List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
 
-        String text;
+        String[] text;
         for(int i = 0; i < products.size();i++){
-            text = products.get(i).getText();
-            if(text.contains("Cucumber")){
-                List<WebElement> cartbuttons = driver.findElements(By.xpath("//button[text()='ADD TO CART']"));
-                cartbuttons.get(i).click();
-                break;
+            text = products.get(i).getText().split("-");
+            String formattedName = text[0].trim();
+
+
+            List<String> veggies = Arrays.asList(vegetablesToFind);
+
+            int veggieCount = 0;
+            if(veggies.contains(formattedName)){
+                veggieCount++;
+                driver.findElements(By.xpath("//div[@class='product-action'] //button")).get(i).click();
+                if(veggieCount == veggies.size()){
+                    break;
+                }
             }
         }
     }
-
+//Check whether the name you extracted is present or not.
+    //trim the product name out of the element containing it's text.
+    //convert Array of veggies into ArrayList to search it more easily.
+    //?? Why are we starting as an array? Arrays use less memory. ArrayList is for more complex operations.
 
 }
