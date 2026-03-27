e4bbddb4b7a6c0d8108885234c606c026cb73f2e
Adam <algolec1990@yahoo.com>
2026-03-27 19:09:05 -0400

    add method addItem into code to optimize test.

--- src/main/java/org/example/ecommerce/AddItemsToCartTest.java
+++ src/main/java/org/example/ecommerce/AddItemsToCartTest.java
@@ -18,30 +18,32 @@ public class AddItemsToCartTest extends Config {
         driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
         String[] vegetablesToFind = {"Brocolli","Cucumber","Beetroot"};
 
-        //Add Cucumber into the cart. Generically setup the test so we can chose any item?
-        List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
-
-        String[] text;
-        for(int i = 0; i < products.size();i++){
-            text = products.get(i).getText().split("-");
-            String formattedName = text[0].trim();
-
-
-            List<String> veggies = Arrays.asList(vegetablesToFind);
-
-            int veggieCount = 0;
-            if(veggies.contains(formattedName)){
-                veggieCount++;
-                driver.findElements(By.xpath("//div[@class='product-action'] //button")).get(i).click();
-                if(veggieCount == veggies.size()){
-                    break;
-                }
-            }
-        }
+        addItems(driver,vegetablesToFind);
     }
 //Check whether the name you extracted is present or not.
     //trim the product name out of the element containing it's text.
     //convert Array of veggies into ArrayList to search it more easily.
     //?? Why are we starting as an array? Arrays use less memory. ArrayList is for more complex operations.
+public void addItems(WebDriver driver,String[] itemsToFind){
+    //Add Cucumber into the cart. Generically setup the test so we can chose any item?
+    List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
 
+    String[] text;
+    for(int i = 0; i < products.size();i++){
+        text = products.get(i).getText().split("-");
+        String formattedName = text[0].trim();
+
+
+        List<String> veggies = Arrays.asList(itemsToFind);
+
+        int veggieCount = 0;
+        if(veggies.contains(formattedName)){
+            veggieCount++;
+            driver.findElements(By.xpath("//div[@class='product-action'] //button")).get(i).click();
+            if(veggieCount == veggies.size()){
+                break;
+            }
+        }
+    }
+}
 }
